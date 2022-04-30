c915d4ce5186af4332de9ebe174a434f843c7ccf
Glitch (citrine-pineapple-september) <none>
2022-04-30 17:50:16 +0000

    🔑🌉 Checkpoint ./CtrlManterFilmes.js:10527415/13163

--- CtrlManterFilmes.js
+++ CtrlManterFilmes.js
@@ -6,61 +6,62 @@ import DaoFilme from "/DaoFilme.js";
 import ViewerFilme from "/ViewerFilme.js";
 
 export default class CtrlManterFilmes {
-  
   //-----------------------------------------------------------------------------------------//
 
   //
   // Atributos do Controlador
   //
-  #dao;      // Referência para o Data Access Object para o Store de Filmes
-  #viewer;   // Referência para o gerenciador do viewer 
+  #dao; // Referência para o Data Access Object para o Store de Filmes
+  #viewer; // Referência para o gerenciador do viewer
   #posAtual; // Indica a posição do objeto Filme que estiver sendo apresentado
-  #status;   // Indica o que o controlador está fazendo 
-  
+  #status; // Indica o que o controlador está fazendo
+
   //-----------------------------------------------------------------------------------------//
 
   constructor() {
     this.#dao = new DaoFilme();
     this.#viewer = new ViewerFilme(this);
     this.#posAtual = 1;
-    this.#atualizarContextoNavegacao();    
+    this.#atualizarContextoNavegacao();
   }
-  
+
   //-----------------------------------------------------------------------------------------//
 
   async #atualizarContextoNavegacao() {
     // Guardo a informação que o controlador está navegando pelos dados
     this.#status = Status.NAVEGANDO;
 
-    // Determina ao viewer que ele está apresentando dos dados 
+    // Determina ao viewer que ele está apresentando dos dados
     this.#viewer.statusApresentacao();
-    
+
     // Solicita ao DAO que dê a lista de todos os filmes presentes na base
     let conjFilmes = await this.#dao.obterFilmes();
-    
+
     // Se a lista de filmes estiver vazia
-    if(conjFilmes.length == 0) {
+    if (conjFilmes.length == 0) {
       // Posição Atual igual a zero indica que não há objetos na base
       this.#posAtual = 0;
-      
+
       // Informo ao viewer que não deve apresentar nada
       this.#viewer.apresentar(0, 0, null);
-    }
-    else {
+    } else {
       // Se é necessário ajustar a posição atual, determino que ela passa a ser 1
-      if(this.#posAtual == 0 || this.#posAtual > conjFilmes.length)
+      if (this.#posAtual == 0 || this.#posAtual > conjFilmes.length)
         this.#posAtual = 1;
       // Peço ao viewer que apresente o objeto da posição atual
-      this.#viewer.apresentar(this.#posAtual, conjFilmes.length, conjFilmes[this.#posAtual - 1]);
+      this.#viewer.apresentar(
+        this.#posAtual,
+        conjFilmes.length,
+        conjFilmes[this.#posAtual - 1]
+      );
     }
   }
-  
+
   //-----------------------------------------------------------------------------------------//
 
   async apresentarPrimeiro() {
     let conjFilmes = await this.#dao.obterFilmes();
-    if(conjFilmes.length > 0)
-      this.#posAtual = 1;
+    if (conjFilmes.length > 0) this.#posAtual = 1;
     this.#atualizarContextoNavegacao();
   }
 
@@ -68,8 +69,7 @@ export default class CtrlManterFilmes {
 
   async apresentarProximo() {
     let conjFilmes = await this.#dao.obterFilmes();
-    if(this.#posAtual < conjFilmes.length)
-      this.#posAtual++;
+    if (this.#posAtual < conjFilmes.length) this.#posAtual++;
     this.#atualizarContextoNavegacao();
   }
 
@@ -77,8 +77,7 @@ export default class CtrlManterFilmes {
 
   async apresentarAnterior() {
     let conjFilmes = await this.#dao.obterFilmes();
-    if(this.#posAtual > 1)
-      this.#posAtual--;
+    if (this.#posAtual > 1) this.#posAtual--;
     this.#atualizarContextoNavegacao();
   }
 
@@ -91,95 +90,92 @@ export default class CtrlManterFilmes {
   }
 
   //-----------------------------------------------------------------------------------------//
-  
+
   iniciarIncluir() {
     this.#status = Status.INCLUINDO;
     this.#viewer.statusEdicao(Status.INCLUINDO);
-    // Guardo a informação que o método de efetivação da operação é o método incluir. 
-    // Preciso disto, pois o viewer mandará a mensagem "efetivar" (polimórfica) ao invés de 
+    // Guardo a informação que o método de efetivação da operação é o método incluir.
+    // Preciso disto, pois o viewer mandará a mensagem "efetivar" (polimórfica) ao invés de
     // "incluir"
     this.efetivar = this.incluir;
   }
 
   //-----------------------------------------------------------------------------------------//
-  
+
   iniciarAlterar() {
     this.#status = Status.ALTERANDO;
     this.#viewer.statusEdicao(Status.ALTERANDO);
-    // Guardo a informação que o método de efetivação da operação é o método incluir. 
-    // Preciso disto, pois o viewer mandará a mensagem "efetivar" (polimórfica) ao invés de 
+    // Guardo a informação que o método de efetivação da operação é o método incluir.
+    // Preciso disto, pois o viewer mandará a mensagem "efetivar" (polimórfica) ao invés de
     // "alterar"
     this.efetivar = this.alterar;
   }
 
   //-----------------------------------------------------------------------------------------//
-  
+
   iniciarExcluir() {
     this.#status = Status.EXCLUINDO;
     this.#viewer.statusEdicao(Status.EXCLUINDO);
-    // Guardo a informação que o método de efetivação da operação é o método incluir. 
-    // Preciso disto, pois o viewer mandará a mensagem "efetivar" (polimórfica) ao invés de 
+    // Guardo a informação que o método de efetivação da operação é o método incluir.
+    // Preciso disto, pois o viewer mandará a mensagem "efetivar" (polimórfica) ao invés de
     // "excluir"
     this.efetivar = this.excluir;
   }
 
   //-----------------------------------------------------------------------------------------//
- 
+
   async incluir(cod, titulo, genero, ano) {
-    if(this.#status == Status.INCLUINDO) {
+    if (this.#status == Status.INCLUINDO) {
       try {
         let filme = new Filme(cod, titulo, genero, ano);
-        await this.#dao.incluir(filme); 
+        await this.#dao.incluir(filme);
         this.#status = Status.NAVEGANDO;
         this.#atualizarContextoNavegacao();
-      }
-      catch(e) {
+      } catch (e) {
         alert(e);
       }
-    }    
+    }
   }
 
   //-----------------------------------------------------------------------------------------//
- 
+
   async alterar(cod, titulo, genero, ano) {
-    if(this.#status == Status.ALTERANDO) {
+    if (this.#status == Status.ALTERANDO) {
       try {
-        let filme = await this.#dao.obterFilmePeloCodigo(cod); 
-        if(filme == null) {
+        let filme = await this.#dao.obterFilmePeloCodigo(cod);
+        if (filme == null) {
           alert("Filme com o código " + cod + " não encontrado.");
         } else {
           filme.setTitulo(titulo);
           filme.setGenero(genero);
           filme.setAno(ano);
-          await this.#dao.alterar(filme); 
+          await this.#dao.alterar(filme);
         }
         this.#status = Status.NAVEGANDO;
         this.#atualizarContextoNavegacao();
-      }
-      catch(e) {
+      } catch (e) {
         alert(e);
       }
-    }    
+    }
   }
 
   //-----------------------------------------------------------------------------------------//
- 
+
   async excluir(cod) {
-    if(this.#status == Status.EXCLUINDO) {
+    if (this.#status == Status.EXCLUINDO) {
       try {
-        let filme = await this.#dao.obterFilmePeloCodigo(cod); 
-        if(filme == null) {
+        let filme = await this.#dao.obterFilmePeloCodigo(cod);
+        if (filme == null) {
           alert("Filme com o código " + cod + " não encontrado.");
         } else {
-          await this.#dao.excluir(filme); 
+          await this.#dao.excluir(filme);
         }
         this.#status = Status.NAVEGANDO;
         this.#atualizarContextoNavegacao();
-      }
-      catch(e) {
+      } catch (e) {
         alert(e);
       }
-    }    
+    }
   }
 
   //-----------------------------------------------------------------------------------------//
@@ -198,26 +194,3 @@ export default class CtrlManterFilmes {
 }
 
 //------------------------------------------------------------------------//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
