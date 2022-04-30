3b30ec93645427911fa99bef2eb403da4fee1248
Glitch (citrine-pineapple-september) <none>
2022-04-30 18:49:10 +0000

    ✋🐛 Checkpoint ./index.html:10527415/4834 ./ViewerFilme.js:10527415/15206

--- ViewerFilme.js
+++ ViewerFilme.js
@@ -5,111 +5,114 @@ import ViewerError from "/ViewerError.js";
 //------------------------------------------------------------------------//
 
 export default class ViewerFilme {
-
   #ctrl;
-  
+
   constructor(ctrl) {
     this.#ctrl = ctrl;
-    this.divNavegar  = this.obterElemento('divNavegar'); 
-    this.divComandos = this.obterElemento('divComandos'); 
-    this.divAviso    = this.obterElemento('divAviso'); 
-    this.divDialogo  = this.obterElemento('divDialogo');
+    this.divNavegar = this.obterElemento("divNavegar");
+    this.divComandos = this.obterElemento("divComandos");
+    this.divAviso = this.obterElemento("divAviso");
+    this.divDialogo = this.obterElemento("divDialogo");
 
-    this.btPrimeiro  = this.obterElemento('btPrimeiro');
-    this.btAnterior  = this.obterElemento('btAnterior');
-    this.btProximo   = this.obterElemento('btProximo');
-    this.btUltimo    = this.obterElemento('btUltimo');
+    this.btPrimeiro = this.obterElemento("btPrimeiro");
+    this.btAnterior = this.obterElemento("btAnterior");
+    this.btProximo = this.obterElemento("btProximo");
+    this.btUltimo = this.obterElemento("btUltimo");
 
-    this.btIncluir   = this.obterElemento('btIncluir');
-    this.btExcluir   = this.obterElemento('btExcluir');
-    this.btAlterar   = this.obterElemento('btAlterar');
-    this.btSair      = this.obterElemento('btSair');
+    this.btIncluir = this.obterElemento("btIncluir");
+    this.btExcluir = this.obterElemento("btExcluir");
+    this.btAlterar = this.obterElemento("btAlterar");
+    this.btSair = this.obterElemento("btSair");
 
-    this.btOk        = this.obterElemento('btOk');
-    this.btCancelar  = this.obterElemento('btCancelar');
+    this.btOk = this.obterElemento("btOk");
+    this.btCancelar = this.obterElemento("btCancelar");
 
-    this.tfCodigo = this.obterElemento('tfCodigo');
-    this.tfTitulo       = this.obterElemento('tfTitulo');
-    this.tfGenero      = this.obterElemento('tfGenero');
-    this.tfAno     = this.obterElemento('tfAno');
-      
-    this.btPrimeiro.onclick = fnBtPrimeiro; 
-    this.btProximo.onclick = fnBtProximo; 
-    this.btAnterior.onclick = fnBtAnterior; 
-    this.btUltimo.onclick = fnBtUltimo; 
+    this.tfCodigo = this.obterElemento("tfCodigo");
+    this.tfTitulo = this.obterElemento("tfTitulo");
+    this.tfGenero = this.obterElemento("tfGenero");
+    this.tfAno = this.obterElemento("tfAno");
+    this.tfUrl = this.obterElemento("tfUrl");
 
-    this.btIncluir.onclick = fnBtIncluir; 
-    this.btAlterar.onclick = fnBtAlterar; 
-    this.btExcluir.onclick = fnBtExcluir; 
+    this.btPrimeiro.onclick = fnBtPrimeiro;
+    this.btProximo.onclick = fnBtProximo;
+    this.btAnterior.onclick = fnBtAnterior;
+    this.btUltimo.onclick = fnBtUltimo;
 
-    this.btOk.onclick = fnBtOk; 
-    this.btCancelar.onclick = fnBtCancelar; 
+    this.btIncluir.onclick = fnBtIncluir;
+    this.btAlterar.onclick = fnBtAlterar;
+    this.btExcluir.onclick = fnBtExcluir;
+
+    this.btOk.onclick = fnBtOk;
+    this.btCancelar.onclick = fnBtCancelar;
   }
 
-//------------------------------------------------------------------------//
+  //------------------------------------------------------------------------//
 
   obterElemento(idElemento) {
     let elemento = document.getElementById(idElemento);
-    if(elemento == null) 
-      throw new ViewerError("Não encontrei um elemento com id '" + idElemento + "'");
+    if (elemento == null)
+      throw new ViewerError(
+        "Não encontrei um elemento com id '" + idElemento + "'"
+      );
     // Adicionando o atributo 'viewer' no elemento do Viewer. Isso permitirá
     // que o elemento guarde a referência para o objeto Viewer que o contém.
     elemento.viewer = this;
     return elemento;
   }
 
-//------------------------------------------------------------------------//
-  
-  getCtrl() { 
+  //------------------------------------------------------------------------//
+
+  getCtrl() {
     return this.#ctrl;
   }
 
-//------------------------------------------------------------------------//
-  
-  apresentar(pos, qtde, filme) {    
-    
-    this.configurarNavegacao( pos <= 1 , pos == qtde );   
+  //------------------------------------------------------------------------//
 
-    if(filme == null) {
+  apresentar(pos, qtde, filme) {
+    this.configurarNavegacao(pos <= 1, pos == qtde);
+
+    if (filme == null) {
       this.tfCodigo.value = "";
-      this.tfTitulo.value       = "";
-      this.tfGenero.value      = "";
-      this.tfAno.value     = "";
+      this.tfTitulo.value = "";
+      this.tfGenero.value = "";
+      this.tfAno.value = "";
+      this.tfUrl.value = "";
       this.divAviso.innerHTML = " Número de Filmes: 0";
     } else {
       this.tfCodigo.value = filme.getCodigo();
-      this.tfTitulo.value       = filme.getTitulo();
-      this.tfGenero.value      = filme.getGenero();
-      this.tfAno.value     = filme.getAno();
-      this.divAviso.innerHTML = "Posição: " + pos + " | Número de Filmes: " + qtde;
+      this.tfTitulo.value = filme.getTitulo();
+      this.tfGenero.value = filme.getGenero();
+      this.tfAno.value = filme.getAno();
+      this.divAviso.innerHTML =
+        "Posição: " + pos + " | Número de Filmes: " + qtde;
     }
   }
 
-//------------------------------------------------------------------------//
+  //------------------------------------------------------------------------//
 
   configurarNavegacao(flagInicio, flagFim) {
     this.btPrimeiro.disabled = flagInicio;
-    this.btUltimo.disabled   = flagFim;
-    this.btProximo.disabled  = flagFim;
+    this.btUltimo.disabled = flagFim;
+    this.btProximo.disabled = flagFim;
     this.btAnterior.disabled = flagInicio;
   }
-  
-//------------------------------------------------------------------------//
-  
-  statusEdicao(operacao) { 
+
+  //------------------------------------------------------------------------//
+
+  statusEdicao(operacao) {
     this.divNavegar.hidden = true;
     this.divComandos.hidden = true;
-    this.divDialogo.hidden = false; 
-    
-    if(operacao != Status.EXCLUINDO) {
+    this.divDialogo.hidden = false;
+
+    if (operacao != Status.EXCLUINDO) {
       this.tfTitulo.disabled = false;
       this.tfGenero.disabled = false;
       this.tfAno.disabled = false;
-      this.divAviso.innerHTML = "";      
+      this.divAviso.innerHTML = "";
     } else {
-      this.divAviso.innerHTML = "Deseja excluir este registro?";      
+      this.divAviso.innerHTML = "Deseja excluir este registro?";
     }
-    if(operacao == Status.INCLUINDO) {
+    if (operacao == Status.INCLUINDO) {
       this.tfCodigo.disabled = false;
       this.tfCodigo.value = "";
       this.tfTitulo.value = "";
@@ -118,19 +121,18 @@ export default class ViewerFilme {
     }
   }
 
-//------------------------------------------------------------------------//
-  
-  statusApresentacao() { 
+  //------------------------------------------------------------------------//
+
+  statusApresentacao() {
     this.tfTitulo.disabled = true;
     this.divNavegar.hidden = false;
     this.divComandos.hidden = false;
-    this.divDialogo.hidden = true; 
+    this.divDialogo.hidden = true;
     this.tfCodigo.disabled = true;
     this.tfTitulo.disabled = true;
     this.tfGenero.disabled = true;
     this.tfAno.disabled = true;
   }
-
 }
 
 //------------------------------------------------------------------------//
@@ -141,7 +143,6 @@ function fnBtPrimeiro() {
   // Aqui, o 'this' é o objeto Button. Eu adicionei o atributo 'viewer'
   // no botão para poder executar a instrução abaixo.
   this.viewer.getCtrl().apresentarPrimeiro();
-  
 }
 
 //------------------------------------------------------------------------//
@@ -150,7 +151,6 @@ function fnBtProximo() {
   // Aqui, o 'this' é o objeto Button. Eu adicionei o atributo 'viewer'
   // no botão para poder executar a instrução abaixo.
   this.viewer.getCtrl().apresentarProximo();
-  
 }
 
 //------------------------------------------------------------------------//
@@ -159,7 +159,6 @@ function fnBtAnterior() {
   // Aqui, o 'this' é o objeto Button. Eu adicionei o atributo 'viewer'
   // no botão para poder executar a instrução abaixo.
   this.viewer.getCtrl().apresentarAnterior();
-  
 }
 
 //------------------------------------------------------------------------//
@@ -168,7 +167,6 @@ function fnBtUltimo() {
   // Aqui, o 'this' é o objeto Button. Eu adicionei o atributo 'viewer'
   // no botão para poder executar a instrução abaixo.
   this.viewer.getCtrl().apresentarUltimo();
-  
 }
 //------------------------------------------------------------------------//
 
@@ -184,7 +182,6 @@ function fnBtAlterar() {
   // Aqui, o 'this' é o objeto Button. Eu adicionei o atributo 'viewer'
   // no botão para poder executar a instrução abaixo.
   this.viewer.getCtrl().iniciarAlterar();
-  
 }
 
 //------------------------------------------------------------------------//
@@ -202,29 +199,24 @@ function fnBtOk() {
   const titulo = this.viewer.tfTitulo.value;
   const genero = this.viewer.tfGenero.value;
   const ano = this.viewer.tfAno.value;
-    
+
   // Como defini que o método "efetivar" é um dos métodos incluir, excluir ou alterar
   // não estou precisando colocar os ninhos de IF abaixo.
-  this.viewer.getCtrl().efetivar(codigo, titulo, genero, ano); 
+  this.viewer.getCtrl().efetivar(codigo, titulo, genero, ano);
 
   // if(this.viewer.getCtrl().getStatus() == Status.INCLUINDO) {
-  //  this.viewer.getCtrl().fnEfetivar(codigo, titulo, genero, ano); 
+  //  this.viewer.getCtrl().fnEfetivar(codigo, titulo, genero, ano);
   //} else if(this.viewer.getCtrl().getStatus() == Status.ALTERANDO) {
-  //  this.viewer.getCtrl().alterar(codigo, titulo, genero, ano); 
+  //  this.viewer.getCtrl().alterar(codigo, titulo, genero, ano);
   //} else if(this.viewer.getCtrl().getStatus() == Status.EXCLUINDO) {
-  //  this.viewer.getCtrl().excluir(codigo, titulo, genero, ano); 
+  //  this.viewer.getCtrl().excluir(codigo, titulo, genero, ano);
   //}
 }
 
 //------------------------------------------------------------------------//
 
 function fnBtCancelar() {
-  this.viewer.getCtrl().cancelar(); 
+  this.viewer.getCtrl().cancelar();
 }
 
 //------------------------------------------------------------------------//
-
-
-
-
-
